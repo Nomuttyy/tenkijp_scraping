683ac839cc402f172a43bb0c5a4a5861a5699744
Nomuttyy <b1718940@planet.kanazawa-it.ac.jp>
2021-09-16 16:40:56 +0900

    フォルダー自動作成つけた

--- tenkijp_scraping.py
+++ tenkijp_scraping.py
@@ -1,47 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import re
-import requests
-from bs4 import BeautifulSoup
-import json
-import pandas as pd
+import os
 import datetime
+from pathlib import Path
 
-
-# In[6]:
+import pandas as pd
 
 
 URL = 'https://tenki.jp/forecast/4/20/5610/17210/1hour.html'
 dfs = pd.read_html(URL,encoding = "UTF-8",header=0,index_col=0)
 
-
-# In[9]:
-
-
-# df = pd.read_html(URL, header = 0)
-
-
-# In[11]:
-
-
-dfs[0].head()
-
-
-# In[20]:
-
-
-today = datetime.date.today()
-today = today.strftime("%Y%m%d")
-today = "/home/nomura/Documents/tenkijp_scraping/tenkijp_hakusan/" + today + '.csv'
+basedtime = datetime.date.today()
+folder = Path(__file__).parent / "tenkijp_hakusan" / str(basedtime.year)
+os.makedirs(folder,exist_ok=True)
+folder = folder / str(basedtime.month)
+os.makedirs(folder,exist_ok=True)
+today = folder / f"{basedtime}.csv"
 dfs[0].to_csv(today)
-# today
-
-
-# In[ ]:
 
 
 
--- tenkijp_scraping_dropNaN.py
+++ tenkijp_scraping_dropNaN.py
@@ -1,56 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-from pathlib import Path
-import re
-import requests
-from bs4 import BeautifulSoup
-import json
-import pandas as pd
 import datetime
+import os
+from pathlib import Path
 
-
-# In[3]:
+import pandas as pd
 
 
 URL = 'https://tenki.jp/forecast/4/20/5610/17210/1hour.html'
 dfs = pd.read_html(URL,encoding = "UTF-8",header=0,index_col=0)
-
-
-# In[4]:
-
-
-# df = pd.read_html(URL, header = 0)
-
-
-# In[12]:
-
-
 df = dfs[0]
-
-
-# In[14]:
-
-
 cols = [0,1,2,4,5,7,8,9,10]
 df  = df.iloc[cols]
 
-
-# In[20]:
-
-
-today = datetime.date.today()
-today = today.strftime("%Y%m%d")
-today = Path(__file__).parent / "tenkijp_hakusan_dropNaN" / f"{today}.csv"
-# today = "/home/nomura/Documents/tenkijp_hakusan_dropNaN/" + today + '.csv'
+basedtime = datetime.date.today()
+folder = Path(__file__).parent / "tenkijp_hakusan_dropNaN" / str(basedtime.year)
+os.makedirs(folder,exist_ok=True)
+folder = folder / str(basedtime.month)
+os.makedirs(folder,exist_ok=True)
+today = folder / f"{basedtime}.csv"
 df.to_csv(today)
-# today
-
-
-# In[ ]:
-
-
 
 

38050b7d49b3df81afcfafdef333d32835577af5
Nomuttyy <b1718940@planet.kanazawa-it.ac.jp>
2021-09-17 14:01:27 +0900

    ftp_transport.pyの完成(天気予報)

--- ftp_transport.py
+++ ftp_transport.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
 from ftplib import FTP
+import datetime
 import json
 
+
+basedtime = datetime.date.today()
+localdirtoday = Path(__file__).parent / "tenkijp_hakusan_dropNaN" / str(basedtime.year) / str(basedtime.month) / f"{basedtime}.csv"
+year = f"/ftp/tenkijp_hakusan_dropNaN/{basedtime.year}"
+month = f"{year}/{basedtime.month}"
+today = f"{month}/{basedtime}.csv"
 with open('/home/nomura/Documents/tenkijp_scraping/config.json') as f:
 	conf = json.load(f)
 	ftp = FTP(conf["ip"],conf["username"],conf["password"])
-with open("/home/nomura/Documents/tenkijp_scraping/test.txt", "rb") as f:
-	ftp.storbinary("STOR /ftp/test.txt", f)
-	
-# ローカルパス
-# local_path = '_____ローカル パス_____'
-
-# # サーバーパス
-# server_path = '_____サーバー パス_____'
-
-# # アップロードするファイルリスト
-# file_list = [
-# 	'index.html',
-# 	'/img/photo.jpg'
-# ]
+if year in ftp.nlst("/ftp/tenkijp_hakusan_dropNaN/") :
+	pass
+else :
+	ftp.mkd(year)
+if month in ftp.nlst(year) :
+	pass
+else :
+	ftp.mkd(month)
+if today in ftp.nlst(month) :
+	pass
+else :
+	with open(localdirtoday,"rb") as f:
+		ftp.storbinary(f"STOR {today}",f)
